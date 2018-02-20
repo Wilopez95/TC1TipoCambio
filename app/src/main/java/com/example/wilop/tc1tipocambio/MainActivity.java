a811920e7a7d5082b01ef2f7c47d8d3a75e483eb
Wilopez95 <Wilopez95@gmail.com>
2018-02-20 12:35:37 -0600

    Second commit , new features in java

--- app/src/main/java/com/example/wilop/tc1tipocambio/MainActivity.java
+++ app/src/main/java/com/example/wilop/tc1tipocambio/MainActivity.java
@@ -2,12 +2,50 @@ package com.example.wilop.tc1tipocambio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioButton Rb1 ;
+    private RadioButton Rb2;
+    private EditText EtC;
+    private EditText EtD ;
+    private Button Conv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         Rb1 = findViewById(R.id.radioButton1);
+         Rb2 = findViewById(R.id.radioButton2);
+         EtC = findViewById(R.id.editText1);
+         EtD = findViewById(R.id.editText2);
+         Conv =  findViewById(R.id.button);
+
+    }
+
+
+
+    public void onButtonClicked(View view){
+
+
+    }
+
+    public void ColToDol(int Colones){
+        Log.d("INFO", "ColToDol: ");
+    }
+
+    public void DolToCol(int Dolares){
+        Log.d("INFO", "DolToCol: ");
+
     }
 }
