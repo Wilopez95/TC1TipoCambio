1c22607b81cecfab04ae7f8db0c222e12d55fb48
Wilopez95 <Wilopez95@gmail.com>
2018-02-22 13:24:22 -0600

    Tird commit , new features in java , the proyect is complete , the change type is of 22/2/18 and can change.

--- app/src/main/java/com/example/wilop/tc1tipocambio/MainActivity.java
+++ app/src/main/java/com/example/wilop/tc1tipocambio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.wilop.tc1tipocambio;
 
+import android.opengl.ETC1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,8 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
-
-
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +17,10 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton Rb1 ;
     private RadioButton Rb2;
     private EditText EtC;
-    private EditText EtD ;
+    private TextView RText;
     private Button Conv;
+    private String Monto;
+
 
 
     @Override
@@ -28,14 +31,43 @@ public class MainActivity extends AppCompatActivity {
          Rb1 = findViewById(R.id.radioButton1);
          Rb2 = findViewById(R.id.radioButton2);
          EtC = findViewById(R.id.editText1);
-         EtD = findViewById(R.id.editText2);
+         RText = findViewById(R.id.textView);
          Conv =  findViewById(R.id.button);
+
+
 
     }
 
+    public void onRadioButton1Clicked(View view){
+        Rb2.setChecked(false);
 
+    }
+    public void onRadioButton2Clicked(View view){
+        Rb1.setChecked(false);
+
+    }
 
     public void onButtonClicked(View view){
+        Monto=EtC.getText().toString();
+        if(Monto.isEmpty()){
+            Toast.makeText (MainActivity.this,"Inserte un monto para convertir", Toast.LENGTH_SHORT).show();
+            RText.setVisibility(View.INVISIBLE);
+        }else{
+            if(Rb1.isChecked()){
+                //Do Colones to Dolars
+                int iMonto =  Integer.parseInt(Monto.toString());
+                double Resultado = iMonto / 572.61;
+                RText.setText( String.format( " %.2f", Resultado )+" Dolares");
+                RText.setVisibility(View.VISIBLE);
+            }else {
+                //Do Dolares to Colones
+                int iMonto =  Integer.parseInt(Monto.toString());
+                double Resultado = iMonto * 572.61;
+                RText.setText(String.format( "%.2f", Resultado )+" Colones");
+                RText.setVisibility(View.VISIBLE);
+            }
+        }
+
 
 
     }
